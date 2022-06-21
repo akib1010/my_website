4e0ce0806c5b988d14784b9cf1585b31f24daea7
Farhan A Rahman <akib1010@gmail.com>
2022-06-21 17:28:05 -0500

    fix home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import {Link} from "react-scroll";
 
 const Home = () => {
   return (
-    <div name="Home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800 text-white">
+    <div name="Home" className="h-screen w-full margin-auto bg-gradient-to-b from-black to-gray-800 text-white">
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
             <div className="flex flex-col justify-center h-full">
                 <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm an aspiring Web/Software Developer</h2>
