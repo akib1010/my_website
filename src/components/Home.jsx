b3b822e5da323a808e677a4dd65ff0b555586ee2
Farhan A Rahman <akib1010@gmail.com>
2022-06-21 17:04:55 -0500

    fix home page description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div name="Home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800 text-white">
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
             <div className="flex flex-col justify-center h-full">
-                <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm an aspiring Web Developer</h2>
+                <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm an aspiring Web/Software Developer</h2>
                 <p className="text-gray-500 py-4 max-2-md">
                     I am a recent graduate with a degree in Computer Science.
                     Currently, I am working on improving my web development skills using technologies such as:
