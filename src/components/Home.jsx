4a333731ab4321793cfac76956ee726b8dfc9c4a
Farhan A Rahman <akib1010@gmail.com>
2022-06-22 14:25:32 -0500

    minor adjustments

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,10 +5,10 @@ import {Link} from "react-scroll";
 
 const Home = () => {
   return (
-    <div name="Home" className="h-screen w-full margin-auto bg-gradient-to-b from-black to-gray-800 text-white">
+    <div name="Home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
             <div className="flex flex-col justify-center h-full">
-                <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm an aspiring Web/Software Developer</h2>
+                <h2 className="text-4xl sm:text-7xl font-bold text-white ">I'm an aspiring Web/Software Developer</h2>
                 <p className="text-gray-500 py-4 max-2-md">
                     I am a recent graduate with a degree in Computer Science.
                     Currently, I am working on improving my web development skills using technologies such as:
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -43,7 +43,7 @@ const Portfolio = () => {
 
 
   return (
-    <div name="Projects" className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
+    <div name="Projects" className="bg-gradient-to-b from-black to-gray-800 h-full w-full margin-auto text-white">
         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
             <div className="pb-8">
                 <p className="text-4xl font-bold inline border-b-4 border-gray-500">Projects</p>
